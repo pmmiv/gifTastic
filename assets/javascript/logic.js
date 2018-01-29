3fd47db95b5b9651285ff8719b86cd2245c2178d
pmmiv <pmmilamiv@gmail.com>
2018-01-29 16:14:19 -0600

    some basic setup

---
+++ assets/javascript/logic.js
@@ -0,0 +1,5 @@
+var btns = ['friends', 'seinfeld', 'the office', 'all my children', 'law and order', 'mad men', 'scrubs', 'saturday night live', 'modern family', 'spongebob', 'tom and jerry', 'king of queens'];
+
+// example request. I'll need to convert spaces to +
+// "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=oGxiCi75V8lJIYCKLIoXGgRp6fG5Audl&limit=20"
+
