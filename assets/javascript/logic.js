ebe1cdbbe9a4b137270beded5e689e964a4d17ea
PMMIV <pmmilamiv@gmail.com>
2018-01-30 08:15:20 -0600

    the site now generates buttons when usr adds them and will generate gifs when the btns are clicked.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,54 @@
-var btns = ['friends', 'seinfeld', 'the office', 'all my children', 'law and order', 'mad men', 'scrubs', 'saturday night live', 'modern family', 'spongebob', 'tom and jerry', 'king of queens'];
+var btns = ['saturday night live', 'the office', 'all my children', 'law and order', 'mad men', 'scrubs', 'seinfeld', 'friends'];
 
-// example request. I'll need to convert spaces to +
-// "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=oGxiCi75V8lJIYCKLIoXGgRp6fG5Audl&limit=20"
+function searchGiphy (show) {
+	// var queryURL = "http://api.giphy.com/v1/gifs/search?q="+show+"&api_key=oGxiCi75V8lJIYCKLIoXGgRp6fG5Audl&limit=20"
+	$.ajax({
+		url: "http://api.giphy.com/v1/gifs/search?q="+show+"&api_key=oGxiCi75V8lJIYCKLIoXGgRp6fG5Audl&limit=10",
+		method: "GET"
+	}).then(function(response) {
+		console.log(response);
+		for (g=0;g<response.data.length;g++) {
+			var gif = $('<img>');
+			gif.attr('src', response.data[g].images.fixed_height.url);
+			$("#gifBox").append(gif);
+		}
+	})
+}
 
+// destroys and recreates all the buttons in the btns array
+function genGifBtn (userInput) {
+	if (!btns.includes(userInput)) {
+		btns.push(userInput)	
+	}
+	$("#btnBox").empty();
+	for (i=0;i<btns.length;i++) {	
+		var gifBtn = $("<button>");
+		gifBtn.addClass("gifBtn btn");
+		gifBtn.attr("value", btns[i])
+		gifBtn.text(btns[i]);
+		$("#btnBox").append(gifBtn);
+	}
+}
+
+$(document).ready(function () {
+	// when a gifBtn is clicked it requests a giphy search and generates the gifs
+	$(document).on('click', '.gifBtn', function () {
+		$('#gifBox').empty();
+		searchGiphy(this.value);
+	})
+	// $(".gifBtn").click(function () {
+	// })
+
+	// when the add button is pressed it generates a new button for the user input
+	$("#add").click(function(event) {
+		event.preventDefault();
+		genGifBtn($("#userInput").val().trim());
+	})
+
+	// generates the initial list of buttons by passing a value that already exists in the array through the button generator function 
+	genGifBtn("friends");
+
+	// onclick .gif
+		// if not playing loop
+		// else if playing stop and reset
+})
